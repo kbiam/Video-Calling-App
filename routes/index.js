8998b151185792f45619f8bfbe388b97c5fbffd0
Kush <kushbang123@gmail.com>
2024-03-20 20:50:38 +0530

    made changes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/chatroom/:room',function(req,res){
-  res.render('chatroom');
+  res.render('chatroom2');
 });
+function checkAccessToIndex(req, res, next) {
+  // Check if the user has accessed '/' route
+  if (req.originalUrl === '/') {
+    // If user has accessed '/', allow them to proceed to the next middleware or route handler
+    next();
+  } else {
+    // If user has not accessed '/', redirect them to '/'
+    res.redirect('/');
+  }
+}
 module.exports = router;
